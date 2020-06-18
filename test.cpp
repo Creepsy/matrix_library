2be5ac685fe8a607e62a2fc0fe8823d7fd6db9bc
calvinreu <calvinreu@localhost.localdomain>
2020-06-18 17:21:20 +0200

    test for lib_v2

--- test.cpp
+++ test.cpp
@@ -3,19 +3,31 @@
 #include <random>
 
 #include "lib_v1/matrix.h"
+#include "lib_v2/matrix"
 
-int main() {
-    ml::matrix<float> a = {
-        {3, 2, 1},
-        {1, 0, 2},
+int main(int argc, char const *argv[]) {
+    
+    
+
+    std::cout << "test 2 started \n";
+
+    extended::matrix<double> a = {
+        {0.9, 1.1, 1.2},
+        {1.3, 1.4, 0.8}
     };
-
-    ml::matrix<float> b = {
-        {1, 2},
-        {0, 1},
-        {4, 0}
+    extended::matrix<double> b;
+    b = {
+        {1, 0, 0},
+        {0, 1, 0},
+        {0, 0, 1}
     };
+    auto c = a*b;
 
-    std::cout << a % b << std::endl;
+    std::cout << c.row_size() << std::endl;
+
+    for (auto i = c.begin(); i < c.end(); i++)
+        std::cout << *i << std::endl;
+    
     return 0;
 }
+
