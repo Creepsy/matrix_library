81a688eb6e726bb0fb9f623c4bc3ebe416f116a1
Creepsy <Robbe-online@gmx.de>
2020-06-18 19:03:15 +0200

    .

--- lib_v1/matrix_iter.cpp
+++ lib_v1/matrix_iter.cpp
@@ -1,1 +1,258 @@
 #include "matrix_iter.h"
+
+template <class typ>
+ml::matrix_iterator<typ>::matrix_iterator() : curr(nullptr) {}
+
+template <class typ>
+ml::matrix_iterator<typ>::matrix_iterator(typ* start) : curr(start) {}
+
+template <class typ>
+bool ml::matrix_iterator<typ>::operator==(const matrix_iterator<typ>& other) const {
+    return this -> curr == other.curr;
+}
+
+template <class typ>
+bool ml::matrix_iterator<typ>::operator!=(const matrix_iterator<typ>& other) const {
+    return !(*this == other);
+}
+
+template <class typ>
+bool ml::matrix_iterator<typ>::operator>(const matrix_iterator<typ>& other) const {
+    return this -> curr > other.curr;
+}
+
+template <class typ>
+bool ml::matrix_iterator<typ>::operator<(const matrix_iterator<typ>& other) const {
+    return this -> curr < other.curr;
+}
+
+template <class typ>
+bool ml::matrix_iterator<typ>::operator>=(const matrix_iterator<typ>& other) const {
+    return (*this > other || *this == other);
+}
+
+template <class typ>
+bool ml::matrix_iterator<typ>::operator<=(const matrix_iterator<typ>& other) const {
+    return (*this < other || *this == other);
+}
+
+template <class typ>
+typ& ml::matrix_iterator<typ>::operator*() {
+    return *this -> curr;
+}
+
+template <class typ>
+ml::matrix_iterator<typ>& ml::matrix_iterator<typ>::operator++() {
+    this -> curr++;
+    return *this;
+}
+
+template <class typ>
+ml::matrix_iterator<typ> ml::matrix_iterator<typ>::operator++(int) {
+    matrix_iterator<typ> ret = *this;
+    ++*this;
+    return ret;
+}
+
+template <class typ>
+ml::matrix_iterator<typ>& ml::matrix_iterator<typ>::operator--() {
+    this -> curr--;
+    return *this;
+}
+
+template <class typ>
+ml::matrix_iterator<typ> ml::matrix_iterator<typ>::operator--(int) {
+    matrix_iterator<typ> ret = *this;
+    --*this;
+    return ret;
+}
+
+template <class typ>
+ml::matrix_iterator<typ> ml::matrix_iterator<typ>::operator-(const matrix_iterator<typ>& other) const {
+    matrix_iterator<typ> ret = *this;
+    ret -= other;
+    return ret;
+}
+
+template <class typ>
+ml::matrix_iterator<typ> ml::matrix_iterator<typ>::operator+(const matrix_iterator<typ>& other) const {
+    matrix_iterator<typ> ret = *this;
+    ret += other;
+    return ret;
+}
+
+template <class typ>
+ml::matrix_iterator<typ> ml::matrix_iterator<typ>::operator-(const int val) const {
+    matrix_iterator<typ> ret = *this;
+    ret -= val;
+    return ret;  
+}
+
+template <class typ>
+ml::matrix_iterator<typ> ml::matrix_iterator<typ>::operator+(const int val) const {
+    matrix_iterator<typ> ret = *this;
+    ret += val;
+    return ret;
+}
+
+template <class typ>
+ml::matrix_iterator<typ>& ml::matrix_iterator<typ>::operator-=(const matrix_iterator<typ>& other){
+    this -> curr -= other.curr;
+    return *this;
+}
+
+template <class typ>
+ml::matrix_iterator<typ>& ml::matrix_iterator<typ>::operator+=(const matrix_iterator<typ>& other){
+    this -> curr += other.curr;
+    return *this;
+}
+
+template <class typ>
+ml::matrix_iterator<typ>& ml::matrix_iterator<typ>::operator-=(const int val){
+    this -> curr -= val;
+    return *this;  
+}
+
+template <class typ>
+ml::matrix_iterator<typ>& ml::matrix_iterator<typ>::operator+=(const int val){
+    this -> curr += val;
+    return *this;
+}
+
+template <class typ>
+typ& ml::matrix_iterator<typ>::operator[](size_t i) {
+    return this -> curr[i];
+}
+
+template <class typ>
+ml::matrix_iterator<typ>::~matrix_iterator() {}
+
+
+
+
+template <class typ>
+ml::const_matrix_iterator<typ>::const_matrix_iterator() : curr(nullptr) {}
+
+template <class typ>
+ml::const_matrix_iterator<typ>::const_matrix_iterator(typ const * start) : curr(start) {}
+
+template <class typ>
+bool ml::const_matrix_iterator<typ>::operator==(const const_matrix_iterator<typ>& other) const {
+    return this -> curr == other.curr;
+}
+
+template <class typ>
+bool ml::const_matrix_iterator<typ>::operator!=(const const_matrix_iterator<typ>& other) const {
+    return !(*this == other);
+}
+
+template <class typ>
+bool ml::const_matrix_iterator<typ>::operator>(const const_matrix_iterator<typ>& other) const {
+    return this -> curr > other.curr;
+}
+
+template <class typ>
+bool ml::const_matrix_iterator<typ>::operator<(const const_matrix_iterator<typ>& other) const {
+    return this -> curr < other.curr;
+}
+
+template <class typ>
+bool ml::const_matrix_iterator<typ>::operator>=(const const_matrix_iterator<typ>& other) const {
+    return (*this > other || *this == other);
+}
+
+template <class typ>
+bool ml::const_matrix_iterator<typ>::operator<=(const const_matrix_iterator<typ>& other) const {
+    return (*this < other || *this == other);
+}
+
+template <class typ>
+const typ& ml::const_matrix_iterator<typ>::operator*() const {
+    return *this -> curr;
+}
+
+template <class typ>
+ml::const_matrix_iterator<typ>& ml::const_matrix_iterator<typ>::operator++() {
+    this -> curr++;
+    return *this;
+}
+
+template <class typ>
+ml::const_matrix_iterator<typ> ml::const_matrix_iterator<typ>::operator++(int) {
+    const_matrix_iterator<typ> ret = *this;
+    ++*this;
+    return ret;
+}
+
+template <class typ>
+ml::const_matrix_iterator<typ>& ml::const_matrix_iterator<typ>::operator--() {
+    this -> curr--;
+    return *this;
+}
+
+template <class typ>
+ml::const_matrix_iterator<typ> ml::const_matrix_iterator<typ>::operator--(int) {
+    const_matrix_iterator<typ> ret = *this;
+    --*this;
+    return ret;
+}
+
+template <class typ>
+ml::const_matrix_iterator<typ> ml::const_matrix_iterator<typ>::operator-(const const_matrix_iterator<typ>& other) const {
+    const_matrix_iterator<typ> ret = *this;
+    ret -= other;
+    return ret;
+}
+
+template <class typ>
+ml::const_matrix_iterator<typ> ml::const_matrix_iterator<typ>::operator+(const const_matrix_iterator<typ>& other) const {
+    const_matrix_iterator<typ> ret = *this;
+    ret += other;
+    return ret;
+}
+
+template <class typ>
+ml::const_matrix_iterator<typ> ml::const_matrix_iterator<typ>::operator-(const int val) const {
+    const_matrix_iterator<typ> ret = *this;
+    ret -= val;
+    return ret;  
+}
+
+template <class typ>
+ml::const_matrix_iterator<typ> ml::const_matrix_iterator<typ>::operator+(const int val) const {
+    const_matrix_iterator<typ> ret = *this;
+    ret += val;
+    return ret;
+}
+
+template <class typ>
+ml::const_matrix_iterator<typ>& ml::const_matrix_iterator<typ>::operator-=(const const_matrix_iterator<typ>& other){
+    this -> curr -= other.curr;
+    return *this;
+}
+
+template <class typ>
+ml::const_matrix_iterator<typ>& ml::const_matrix_iterator<typ>::operator+=(const const_matrix_iterator<typ>& other){
+    this -> curr += other.curr;
+    return *this;
+}
+
+template <class typ>
+ml::const_matrix_iterator<typ>& ml::const_matrix_iterator<typ>::operator-=(const int val){
+    this -> curr -= val;
+    return *this;  
+}
+
+template <class typ>
+ml::const_matrix_iterator<typ>& ml::const_matrix_iterator<typ>::operator+=(const int val){
+    this -> curr += val;
+    return *this;
+}
+
+template <class typ>
+const typ& ml::const_matrix_iterator<typ>::operator[](size_t i) const {
+    return this -> curr[i];
+}
+
+template <class typ>
+ml::const_matrix_iterator<typ>::~const_matrix_iterator() {}
--- lib_v1/matrix_iter.h
+++ lib_v1/matrix_iter.h
@@ -5,113 +5,40 @@
 namespace ml
 {
     template <class typ>
-    class matrix_iterator : public std::iterator<std::random_access_iterator_tag, typ, size_t, typ*, typ&>
-    {
+    class matrix_iterator : public std::iterator<std::random_access_iterator_tag, typ, size_t, typ*, typ&> {
     private:
         typ* curr;
     public:
-        matrix_iterator() : curr(nullptr) {}
-        matrix_iterator(typ* start) : curr(start) {}
+        matrix_iterator();
+        matrix_iterator(typ* start);
 
-        bool operator==(const matrix_iterator<typ>& other) const {
-            return this -> curr == other.curr;
-        }
+        bool operator==(const matrix_iterator<typ>& other) const;
+        bool operator!=(const matrix_iterator<typ>& other) const;
+        bool operator>(const matrix_iterator<typ>& other) const;
+        bool operator<(const matrix_iterator<typ>& other) const;
+        bool operator>=(const matrix_iterator<typ>& other) const;
+        bool operator<=(const matrix_iterator<typ>& other) const;
 
-        bool operator!=(const matrix_iterator<typ>& other) const {
-            return !(*this == other);
-        }
+        typ& operator*();
 
-        bool operator>(const matrix_iterator<typ>& other) const {
-            return this -> curr > other.curr;
-        }
+        matrix_iterator<typ>& operator++();
+        matrix_iterator<typ> operator++(int);
+        matrix_iterator<typ>& operator--();
+        matrix_iterator<typ> operator--(int);
 
-        bool operator<(const matrix_iterator<typ>& other) const {
-            return this -> curr < other.curr;
-        }
+        matrix_iterator<typ> operator-(const matrix_iterator<typ>& other) const;
+        matrix_iterator<typ> operator+(const matrix_iterator<typ>& other) const;
+        matrix_iterator<typ> operator-(const int val) const;
+        matrix_iterator<typ> operator+(const int val) const;
+        
+        matrix_iterator<typ>& operator-=(const matrix_iterator<typ>& other);
+        matrix_iterator<typ>& operator+=(const matrix_iterator<typ>& other);
+        matrix_iterator<typ>& operator-=(const int val);
+        matrix_iterator<typ>& operator+=(const int val);
 
-        bool operator>=(const matrix_iterator<typ>& other) const {
-            return (*this > other || *this == other);
-        }
+        typ& operator[](size_t i);
 
-        bool operator<=(const matrix_iterator<typ>& other) const {
-            return (*this < other || *this == other);
-        }
-
-        typ& operator*() {
-            return *this -> curr;
-        }
-
-        matrix_iterator<typ>& operator++() {
-            this -> curr++;
-            return *this;
-        }
-
-        matrix_iterator<typ> operator++(int) {
-            matrix_iterator<typ> ret = *this;
-            ++*this;
-            return ret;
-        }
-
-        matrix_iterator<typ>& operator--() {
-            this -> curr--;
-            return *this;
-        }
-
-        matrix_iterator<typ> operator--(int) {
-            matrix_iterator<typ> ret = *this;
-            --*this;
-            return ret;
-        }
-
-        matrix_iterator<typ> operator-(const matrix_iterator<typ>& other) const {
-            matrix_iterator<typ> ret = *this;
-            ret -= other;
-            return ret;
-        }
-
-        matrix_iterator<typ> operator+(const matrix_iterator<typ>& other) const {
-            matrix_iterator<typ> ret = *this;
-            ret += other;
-            return ret;
-        }
-
-        matrix_iterator<typ> operator-(const int val) const {
-            matrix_iterator<typ> ret = *this;
-            ret -= val;
-            return ret;  
-        }
-
-        matrix_iterator<typ> operator+(const int val) const {
-            matrix_iterator<typ> ret = *this;
-            ret += val;
-            return ret;
-        }
-
-        matrix_iterator<typ>& operator-=(const matrix_iterator<typ>& other){
-            this -> curr -= other.curr;
-            return *this;
-        }
-
-        matrix_iterator<typ>& operator+=(const matrix_iterator<typ>& other){
-            this -> curr += other.curr;
-            return *this;
-        }
-
-        matrix_iterator<typ>& operator-=(const int val){
-            this -> curr -= val;
-            return *this;  
-        }
-
-        matrix_iterator<typ>& operator+=(const int val){
-            this -> curr += val;
-            return *this;
-        }
-
-        typ& operator[](size_t i) {
-            return this -> curr[i];
-        }
-
-        ~matrix_iterator() {}
+        ~matrix_iterator();
     };
 
     template <class typ>
@@ -120,107 +47,34 @@ namespace ml
     private:
         typ const * curr;
     public:
-        const_matrix_iterator() : curr(nullptr) {}
-        const_matrix_iterator(typ const * start) : curr(start) {}
+        const_matrix_iterator();
+        const_matrix_iterator(typ const * start);
 
-        bool operator==(const const_matrix_iterator<typ>& other) const {
-            return this -> curr == other.curr;
-        }
+        bool operator==(const const_matrix_iterator<typ>& other) const;
+        bool operator!=(const const_matrix_iterator<typ>& other) const;
+        bool operator>(const const_matrix_iterator<typ>& other) const;
+        bool operator<(const const_matrix_iterator<typ>& other) const;
+        bool operator>=(const const_matrix_iterator<typ>& other) const; 
+        bool operator<=(const const_matrix_iterator<typ>& other) const; 
 
-        bool operator!=(const const_matrix_iterator<typ>& other) const {
-            return !(*this == other);
-        }
+        const typ& operator*() const;
 
-        bool operator>(const const_matrix_iterator<typ>& other) const {
-            return this -> curr > other.curr;
-        }
+        const_matrix_iterator<typ>& operator++();
+        const_matrix_iterator<typ> operator++(int); 
+        const_matrix_iterator<typ>& operator--();
+        const_matrix_iterator<typ> operator--(int); 
 
-        bool operator<(const const_matrix_iterator<typ>& other) const {
-            return this -> curr < other.curr;
-        }
+        const_matrix_iterator<typ> operator-(const const_matrix_iterator<typ>& other) const;
+        const_matrix_iterator<typ> operator+(const const_matrix_iterator<typ>& other) const;
+        const_matrix_iterator<typ> operator-(const int val) const;
+        const_matrix_iterator<typ> operator+(const int val) const;
 
-        bool operator>=(const const_matrix_iterator<typ>& other) const {
-            return (*this > other || *this == other);
-        }
+        const_matrix_iterator<typ>& operator-=(const const_matrix_iterator<typ>& other);
+        const_matrix_iterator<typ>& operator+=(const const_matrix_iterator<typ>& other);
+        const_matrix_iterator<typ>& operator-=(const int val);
+        const_matrix_iterator<typ>& operator+=(const int val);
 
-        bool operator<=(const const_matrix_iterator<typ>& other) const {
-            return (*this < other || *this == other);
-        }
-
-        const typ& operator*() const {
-            return *this -> curr;
-        }
-
-        const_matrix_iterator<typ>& operator++() {
-            this -> curr++;
-            return *this;
-        }
-
-        const_matrix_iterator<typ> operator++(int) {
-            const_matrix_iterator<typ> ret = *this;
-            ++*this;
-            return ret;
-        }
-
-        const_matrix_iterator<typ>& operator--() {
-            this -> curr--;
-            return *this;
-        }
-
-        const_matrix_iterator<typ> operator--(int) {
-            const_matrix_iterator<typ> ret = *this;
-            --*this;
-            return ret;
-        }
-
-        const_matrix_iterator<typ> operator-(const const_matrix_iterator<typ>& other) const {
-            const_matrix_iterator<typ> ret = *this;
-            ret -= other;
-            return ret;
-        }
-
-        const_matrix_iterator<typ> operator+(const const_matrix_iterator<typ>& other) const {
-            const_matrix_iterator<typ> ret = *this;
-            ret += other;
-            return ret;
-        }
-
-        const_matrix_iterator<typ> operator-(const int val) const {
-            const_matrix_iterator<typ> ret = *this;
-            ret -= val;
-            return ret;  
-        }
-
-        const_matrix_iterator<typ> operator+(const int val) const {
-            const_matrix_iterator<typ> ret = *this;
-            ret += val;
-            return ret;
-        }
-
-        const_matrix_iterator<typ>& operator-=(const const_matrix_iterator<typ>& other){
-            this -> curr -= other.curr;
-            return *this;
-        }
-
-        const_matrix_iterator<typ>& operator+=(const const_matrix_iterator<typ>& other){
-            this -> curr += other.curr;
-            return *this;
-        }
-
-        const_matrix_iterator<typ>& operator-=(const int val){
-            this -> curr -= val;
-            return *this;  
-        }
-
-        const_matrix_iterator<typ>& operator+=(const int val){
-            this -> curr += val;
-            return *this;
-        }
-
-        const typ& operator[](size_t i) const {
-            return this -> curr[i];
-        }
-
-        ~const_matrix_iterator() {}
+        const typ& operator[](size_t i) const;
+        ~const_matrix_iterator();
     };
 }
